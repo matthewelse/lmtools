01cad1140621b7cc51bbc85996fbd0a4b5d7a153
Matthew Else <matthewelse1997@arm.com>
2014-07-29 10:50:19 +0100

    Added more command line options for lm.py

--- scripts/lm.py
+++ scripts/lm.py
@@ -7,8 +7,11 @@ if __name__ == "__main__":
 	# Output list of currently connected microcontrollers.
 
 	parser = argparse.ArgumentParser(description='List all connected mbed-compatible microcontrollers')
-	parser.add_argument('--json', dest='json', type=argparse.FileType('r'), help='Location of JSON file containing mapping between codes and devices.')
-	parser.add_argument('--micro', dest='micro', help='Find a specific microcontroller.')
+	parser.add_argument('-j', dest='json', type=argparse.FileType('r'), help='Location of JSON file containing mapping between codes and devices.')
+	parser.add_argument('-i', dest='micro', help='Find a specific microcontroller by name.')
+	parser.add_argument('-p', dest='port', help='Find a specific microcontroller by serial port.')
+	parser.add_argument('-m', dest='mount_point', help='Find a specific microcontroller by mount point.')
+	parser.add_argument('-n', dest='code', help='Find a specific microcontroller by serial number.')
 
 	args = parser.parse_args()
 	
@@ -18,11 +21,23 @@ if __name__ == "__main__":
 	if args.micro is not None:
 		devices = {k: v for k, v in devices.items() if v['name'] == args.micro}
 
-	x = PrettyTable(["Name", "Port", "Drive", "Serial Number"])
-	x.align["Name"] = 'l'
-	x.align["Serial Number"] = 'l'
+	if args.port is not None:
+		devices = {k: v for k, v in devices.items() if v['port'] == args.port}
 
-	for k, v in devices.items():
-		x.add_row([v["name"], v["port"], v["mountpoint"], k])
+	if args.mount_point is not None:
+		devices = {k: v for k, v in devices.items() if v['mount_point'] == args.mount_point}
 
-	print x
+	if args.code is not None:
+		devices = {k: v for k, v in devices.items() if k == args.code}
+
+	if len(devices) > 0:
+		x = PrettyTable(["Name", "Port", "Drive", "Serial Number"])
+		x.align["Name"] = 'l'
+		x.align["Serial Number"] = 'l'
+
+		for k, v in devices.items():
+			x.add_row([v["name"], v["port"], v["mount_point"], k])
+
+		print x
+	else:
+		print "No devices connected"
